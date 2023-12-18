2dad1b96b34fc58f5b2c142669b7ffd783225940
TuanVo154 <vohoangtuan154@gmail.com>
2023-12-18 17:57:49 +0700

    Refactor code: create router and app/controller folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+const route = require(".\\routers\\index");
+
 app.use(express.static(path.join(__dirname, "\\public")));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 // HTTP logger
 app.use(morgan("combined"));
 
@@ -19,17 +29,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "\\resources\\views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-/*app.post("/", (req, res) => {
-  res.send("");
-});*/
+//Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
