3569ac6af6ebeb5a945c01a370f4e0c5685f4a82
TuanVo154 <vohoangtuan154@gmail.com>
2023-12-20 22:11:27 +0700

    create homepage binding course into homepage

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,28 @@
+const Course = require("../model/Course");
+const { multipleMongooseToObject } = require("../../util/mongoose");
+
 class SiteController {
   //[GET] /Site
-  index(req, res) {
-    res.render('home');
+  // async index(req, res) {
+  //   try {
+  //     const courses = await Course.find({});
+  //     res.json(courses);
+  //   } catch (error) {
+  //     res.status(400).json({ error: "ERROR!!!" });
+  //   }
+  // }
+
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => {
+        res.render("home", { courses: multipleMongooseToObject(courses) });
+      })
+      .catch(next);
   }
 
   // [GET] /search
   search(req, res) {
-    res.render('search');
+    res.render("search");
   }
 }
 
